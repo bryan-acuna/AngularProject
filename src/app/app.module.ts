1be4b9b32f828d3d9d7609d4b3592be0f59bd82d
Bryan Acuna <acuna.b@pg.com>
2021-02-09 17:06:09 -0500

    Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from "@angular/router";
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
+  {path: '', component: ProfileComponent}
 ];
 
 
